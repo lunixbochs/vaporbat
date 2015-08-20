4c320349eec0605443107ae43aff75e041d9b1e9
Ryan Hileman <lunixbochs@gmail.com>
2015-08-20 08:53:12 -0700

    fix login with new protobuf

--- vaporbat/steam/mapping.py
+++ vaporbat/steam/mapping.py
@@ -1,4 +1,4 @@
-from steam.protobuf import steam_server, steam_base
+from steam.protobuf import steam_server, steam_server2, steam_base
 from steamd import EMsg
 import steamd
 
@@ -8,16 +8,13 @@ PROTO_MAPPING = {
     EMsg.ClientCMList: steam_server.CMsgClientCMList,
     EMsg.ClientLogOnResponse: steam_server.CMsgClientLogonResponse,
     EMsg.ClientNewLoginKey: steam_server.CMsgClientNewLoginKey,
-    EMsg.ClientUpdateMachineAuth: steam_server.CMsgClientUpdateMachineAuth,
+    EMsg.ClientUpdateMachineAuth: steam_server2.CMsgClientUpdateMachineAuth,
     EMsg.ClientFriendsList: steam_server.CMsgClientFriendsList,
-    EMsg.ClientEmailAddrInfo: steam_server.CMsgClientEmailAddrInfo,
+    EMsg.ClientEmailAddrInfo: steam_server2.CMsgClientEmailAddrInfo,
     EMsg.ClientAccountInfo: steam_server.CMsgClientAccountInfo,
     EMsg.ClientLicenseList: steam_server.CMsgClientLicenseList,
     EMsg.ClientGameConnectTokens: steam_server.CMsgClientGameConnectTokens,
-    EMsg.ClientFromGC: steam_server.CMsgGCClient,
-    EMsg.EconTrading_InitiateTradeProposed: steam_server.CMsgTrading_InitiateTradeRequest,
-    EMsg.EconTrading_InitiateTradeResult: steam_server.CMsgTrading_InitiateTradeResponse,
-    EMsg.EconTrading_StartSession: steam_server.CMsgTrading_StartSession,
+    EMsg.ClientFromGC: steam_server2.CMsgGCClient,
     EMsg.ClientPersonaState:steam_server.CMsgClientPersonaState,
 }
 WANTS_HEADER = [EMsg.ClientUpdateMachineAuth]
--- vaporbat/steam/protobuf/__init__.py
+++ vaporbat/steam/protobuf/__init__.py
@@ -3,3 +3,4 @@ import encrypted_app_ticket_pb2 as encrypted_app_ticket
 import iclient_objects_pb2 as iclient
 import steammessages_base_pb2 as steam_base
 import steammessages_clientserver_pb2 as steam_server
+import steammessages_clientserver_2_pb2 as steam_server2
