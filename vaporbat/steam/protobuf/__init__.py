ffb3ba18919fdb494356da4f0299e468f447f8d8
Ryan Hileman <lunixbochs@gmail.com>
2015-09-30 20:33:36 -0700

    fix deps and add stream imports

--- vaporbat/steam/protobuf/__init__.py
+++ vaporbat/steam/protobuf/__init__.py
@@ -2,5 +2,6 @@ import content_manifest_pb2 as content_manifest
 import encrypted_app_ticket_pb2 as encrypted_app_ticket
 import iclient_objects_pb2 as iclient
 import steammessages_base_pb2 as steam_base
-import steammessages_clientserver_pb2 as steam_server
 import steammessages_clientserver_2_pb2 as steam_server2
+import steammessages_clientserver_pb2 as steam_server
+import steammessages_remoteclient_discovery_pb2 as steam_remote
