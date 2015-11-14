663cf295769092663af5cca249639fd6d9cd7a1c
Ryan Hileman <lunixbochs@gmail.com>
2015-11-13 22:15:16 -0800

    fix stream discovery broadcast

--- stream.py
+++ stream.py
@@ -1,3 +1,4 @@
+from cStringIO import StringIO
 import socket
 import struct
 import sys
@@ -6,6 +7,7 @@ import time
 from vaporbat.steam.protobuf import steam_remote
 
 client_id = 1234
+PREFIX = 0xA05F4C21FFFFFFFF
 
 def discover():
     hdr = steam_remote.CMsgRemoteClientBroadcastHeader()
@@ -17,7 +19,7 @@ def discover():
     msg.seq_num = 1
     msg = msg.SerializeToString()
 
-    data = struct.pack('<Q', 0xFFFFFFFFA05F4C21)
+    data = struct.pack('<Q', PREFIX)
     data += struct.pack('<I', len(hdr)) + hdr
     data += struct.pack('<I', len(msg)) + msg
 
@@ -27,20 +29,35 @@ def discover():
     s.bind(('0.0.0.0', 27036))
 
     s.settimeout(0.3)
-    while True:
-        print 'broadcasting discovery...'
+    resp = None
+    while not resp:
+        print 'broadcasting discovery...', len(data), data.encode('hex')
         s.sendto(data, ('255.255.255.255', 27036))
-        try:
-            resp, addr = s.recvfrom(1024)
-            if resp:
-                print 'got response', resp
+        while True:
+            try:
+                resp, addr = s.recvfrom(1024)
+                if resp and resp != data:
+                    print 'got response', resp.encode('hex')
+                    break
+                resp = None
+            except socket.timeout:
                 break
-        except socket.timeout:
-            continue
+        time.sleep(0.1)
 
-    r = steam_remote.CMsgRemoteClientBroadcastStatus()
-    r.ParseFromString(resp)
-    return r
+    assert(struct.unpack('<Q', resp[:8]) == (PREFIX,))
+    length, = struct.unpack('<I', resp[8:12])
+    resp = resp[8+4:]
+    hdrb = resp[:length]
+    resp = resp[length:]
+    length, = struct.unpack('<I', resp[:4])
+    msgb = resp[4:4+length]
+
+    hdr = steam_remote.CMsgRemoteClientBroadcastHeader()
+    hdr.ParseFromString(hdrb)
+    assert(hdr.msg_type == steam_remote.k_ERemoteClientBroadcastMsgStatus)
+    msg = steam_remote.CMsgRemoteClientBroadcastStatus()
+    msg.ParseFromString(msgb)
+    return msg
 
 if __name__ == '__main__':
     print discover()
